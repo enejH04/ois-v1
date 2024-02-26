b25848b8e90555eb676cadf627f5a7d3975909f1
denbal2292 <sddebest.music@gmail.com>
2024-02-26 19:42:30 +0100

    Dodaj izračun pospeška

--- gravitacija.py
+++ gravitacija.py
@@ -1,1 +1,10 @@
 print("OIS je zakon!")
+
+def izracun_gravitacije(visina: float) -> float:
+    """
+    Za podano nadmorsko višino izračuna gravitacijski pospešek.
+    """
+    C = 6.674e-11
+    M = 5.5972e24
+    r = 6.371e6
+    return (C * M) / (r + visina ** 2)

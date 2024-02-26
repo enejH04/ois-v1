df64c0aeb537a7dd96a6b62244ca9d24057821f8
OZ13831 <oskar.zerak@gmail.com>
2024-02-26 20:22:03 +0100

    Dodajanje izpisa pospeska #2

--- gravitacija.py
+++ gravitacija.py
@@ -1,1 +1,10 @@
 print("OIS je zakon!")
+
+
+def izpis(visina: float) -> None:
+    """
+    Izpis gravitacijskega pospeška na <visina> kilometrih.
+
+    @param: visina (float): nadmorska višina v kilometrih
+    """
+    print(f"Gravitacijski pospešek na {visina} km nadmorske visine je {izracunGravitacije(visina * 1000):.2f} m/s^2.")

e47e51a41e106bc6f0f43c9ed9b8a5d0d16a8632
denbal2292 <sddebest.music@gmail.com>
2024-02-26 21:14:22 +0100

    Popravi zatipk pri konstanti

--- gravitacija.py
+++ gravitacija.py
@@ -1,13 +1,15 @@
 print("OIS je zakon! Pa se veliko se bomo naucili.")
 
+
 def izracun_gravitacije(visina: float) -> float:
     """
     Za podano nadmorsko višino izračuna gravitacijski pospešek.
     """
     C = 6.674e-11
-    M = 5.5972e24
+    M = 5.972e24
     r = 6.371e6
     return (C * M) / ((r + visina) ** 2)
+
 
 def izpis(visina: float) -> None:
     """
@@ -17,9 +19,11 @@ def izpis(visina: float) -> None:
     """
     print(f"Gravitacijski pospešek na {visina} km nadmorske visine je {izracun_gravitacije(visina * 1000):.2f} m/s^2.")
 
+
 def main():
-    visina = int(input("Vnesite nadmorsko visino v km: "))
+    visina = float(input("Vnesite nadmorsko visino v km: "))
     izpis(visina)
+
 
 if __name__ == "__main__":
     main()
